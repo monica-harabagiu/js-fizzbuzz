fa4bedac9a9411dd18570da180a643d0063ffc6f
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-01-09 16:18:53 +0100

    bonus 1 all elements DOM

--- main.js
+++ main.js
@@ -3,15 +3,25 @@
 // per i multipli di 5 -> Buzz al posto del numero
 // per i multipli sia di 3 che di 5 -> FizzBuzz al posto del numero
 
+const squares = document.getElementById("container")
+
 for (let i = 1; i <= 100; i++) {
 
     if ((i % 3) === 0 && (i % 5) === 0) {
-        console.log("Fizz")
-    } else if ((i % 3) === 0) {
-        console.log("Buzz")
-    } else if ((i % 5) === 0) {
+        const littleSquare = `<div class="fizz-buzz">FizzBuzz</div>`
+        squares.innerHTML += littleSquare
         console.log("FizzBuzz")
+    } else if ((i % 3) === 0) {
+        const littleSquare = `<div class="fizz">Fizz</div>`
+        squares.innerHTML += littleSquare
+        console.log("Fizz")
+    } else if ((i % 5) === 0) {
+        const littleSquare = `<div class="buzz">Buzz</div>`
+        squares.innerHTML += littleSquare
+        console.log("Buzz")
     } else {
+        const littleSquare = `<div class="number">${i}</div>`
+        squares.innerHTML += littleSquare
         console.log(i)
     }
 
